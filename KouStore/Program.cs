96465228dc5ab21d57366c393f24539095ef0c65
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-10 18:50:10 +0300

    created admin area

--- KouStore/Program.cs
+++ KouStore/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
