9025ae6a7a737e7fe7e37a0ce767b462a4cae498
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-12 12:23:15 +0300

    created product model

---
+++ KouStore/Models/ProductModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KouStore.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(200)]
+        public string Description { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        [Range(0, 99999)]
+        public decimal Price { get; set; }
+        [Required]
+        [Range(0,99999)]
+        public uint StockQuantity { get; set; }
+    }
+}
---
+++ KouStore/Models/SubCategoryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace KouStore.Models
+{
+    public class SubCategoryModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public CategoryModel Category { get; set; }
+    }
+}
