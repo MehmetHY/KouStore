da6846622e0a189f31f7e77a5ad8759cbf432fd8
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-13 20:56:08 +0300

    created image model

---
+++ KouStore/Config/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace KouStore.Config
+{
+    public static class Settings
+    {
+        public static int MaxImageSize => 1 * 1024 * 1024;
+    }
+}
---
+++ KouStore/Models/ImageModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static KouStore.Config.Settings;
+
+namespace KouStore.Models
+{
+    public class ImageModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DataType(DataType.Upload)]
+        public IFormFile? Data { get; set; }
+    }
+}
