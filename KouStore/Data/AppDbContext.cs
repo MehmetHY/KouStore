ea271eb5dcf0f0fcc4322aa4b42bb5d08b98b9e4
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-13 20:45:45 +0300

    created admin model

--- KouStore/Data/AppDbContext.cs
+++ KouStore/Data/AppDbContext.cs
@@ -1,5 +1,4 @@
-﻿using KouStore.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace KouStore.Data
 {
@@ -7,38 +6,6 @@ namespace KouStore.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-        }
-
-        public DbSet<AdminModel> Admins { get; set; }
-        public AdminModel? GetAdminByName(string name)
-        {
-            return Admins.FirstOrDefault(a => a.UId == name);
-        }
-
-        public DbSet<ProductModel> Products { get; set; }
-        public List<ProductModel>? GetAllProducts()
-        {
-            return Products.ToList();
-        }
-        public ProductModel? GetProductById(int id)
-        {
-            return Products.FirstOrDefault(p => p.Id == id);
-        }
-
-        public DbSet<CategoryModel> Categories { get; set; }
-        public List<CategoryModel> GetAllCategories()
-        {
-            return Categories.ToList();
-        }
-        
-        public DbSet<SubCategoryModel> SubCategories { get; set; }
-        public List<SubCategoryModel> GetAllSubCategories()
-        {
-            return SubCategories.ToList();
-        }
-        public SubCategoryModel GetSubCategoryById(int id)
-        {
-            return SubCategories.First(s => s.Id == id);
         }
     }
 }
---
+++ KouStore/Models/AdminModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KouStore.Models
+{
+    public class AdminModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
