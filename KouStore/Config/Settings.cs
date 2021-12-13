1c033b885ac054d48376a1bb3a5248bd06eaa60d
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-13 19:46:46 +0300

    reset project

--- KouStore/Config/Settings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KouStore.Config
-{
-    public static class Settings
-    {
-        public enum Language { English, Turkish }
-        public static Language CurrentLanguage { get; set; } = Language.Turkish;
-    }
-}
