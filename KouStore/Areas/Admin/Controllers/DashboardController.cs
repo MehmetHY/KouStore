6775d9f08398743d7d6f9140561aa234ce1de02b
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-14 15:24:48 +0300

    created admin dashboard controller

---
+++ KouStore/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KouStore.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ManageProducts()
+        {
+            return View();
+        }
+        public IActionResult ManageCategories()
+        {
+            return View();
+        }
+        public IActionResult ManageCustomers()
+        {
+            return View();
+        }
+    }
+}
