64176e84b9c123b82ae405d121f1ef0a6b115453
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-14 12:40:50 +0300

    created sign in manager

---
+++ KouStore/Managers/SignInManager.cs
@@ -0,0 +1,11 @@
+﻿namespace KouStore.Managers
+{
+    public static class SignInManager
+    {
+        public const string ADMIN_ID_KEY = "AdminId";
+        public static bool IsAdminSignedIn(ISession session)
+        {
+            return session.GetString(ADMIN_ID_KEY) != null;
+        }
+    }
+}
