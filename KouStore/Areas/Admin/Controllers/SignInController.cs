c6e6685f6811518f1d55f2309488bfbba1b5c641
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-14 12:31:17 +0300

    created admin form model and sign in controller

---
+++ KouStore/Areas/Admin/Controllers/SignInController.cs
@@ -0,0 +1,22 @@
+﻿using KouStore.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KouStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SignInController : Controller
+    {
+        [Route("[Area]/[Controller]")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new AdminFormModel());
+        }
+        [Route("[Area]/[Controller]")]
+        [HttpGet]
+        public IActionResult Index(AdminFormModel? model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ KouStore/Areas/Admin/Models/AdminFormModel.cs
@@ -0,0 +1,13 @@
+﻿using KouStore.Models;
+
+namespace KouStore.Areas.Admin.Models
+{
+    public class AdminFormModel
+    {
+        public AdminModel? Admin { get; set; }
+        public bool NameValid { get; set; } = true;
+        public bool PasswordValid { get; set; } = true;
+        public string? NameErrorMessage { get; set; }
+        public string? PasswordErrorMessage { get; set; }
+    }
+}
