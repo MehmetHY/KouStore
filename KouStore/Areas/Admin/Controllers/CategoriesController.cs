d0ccb56916134cdbdc3fc663244ef815325f7f32
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-16 21:32:40 +0300

    created categories controller

---
+++ KouStore/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,55 @@
+﻿using KouStore.Areas.Admin.Models;
+using KouStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KouStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[Area]/[Controller]/[Action]")]
+    public class CategoriesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(FormModel<CategoryViewModel> formModel)
+        {
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Update(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(FormModel<CategoryViewModel> formModel)
+        {
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(CategoryModel category)
+        {
+            return View();
+        }
+
+
+    }
+}
