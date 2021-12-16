ae23a3413369d02c5d70be4e303aa5331ec193f2
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-16 14:48:46 +0300

    created form model

---
+++ KouStore/Models/FormModel.cs
@@ -0,0 +1,32 @@
+﻿using KouStore.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KouStore.Models
+{
+    public class FormModel
+    {
+        public FormModel(IViewModel viewModel, Controller controller)
+        {
+            ViewModel = viewModel;
+            CurrentController = controller;
+        }
+        public IViewModel ViewModel { get; set; }
+        public Controller CurrentController { get; set; }
+        public string ViewName { get; set; } = string.Empty;
+        public IActionResult? TargetAction { get; set; }
+        public delegate void ViewModelAction(IViewModel model);
+        public ViewModelAction? SuccessAction { get; set; }
+        private bool IsFormValid() => 
+            ViewModel.Result;
+        public IActionResult ProcessForm() 
+        {
+            ViewModel.ValidateViewModel();
+            if (IsFormValid() && SuccessAction != null && TargetAction != null)
+            {
+                SuccessAction(ViewModel);
+                return TargetAction;
+            }
+            return CurrentController.RedirectToAction(ViewName, new { formModel = this });
+        }
+    }
+}
