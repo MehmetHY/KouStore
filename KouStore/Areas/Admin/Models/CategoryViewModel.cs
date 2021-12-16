4547770896c2a472f9c010382ad2c2a52e073ffe
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-16 19:56:56 +0300

    created category view model

---
+++ KouStore/Areas/Admin/Models/CategoryViewModel.cs
@@ -0,0 +1,26 @@
+﻿using KouStore.Data;
+using KouStore.Models;
+using KouStore.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KouStore.Areas.Admin.Models
+{
+    public class CategoryViewModel : IViewModel
+    {
+        public CategoryModel Category { get; set; }
+        public bool NameValid { get; set; } = true;
+        public string NameError { get; set; } = string.Empty;
+        public AppDbContext? DbContext { get; set; }
+        public bool Result => NameValid;
+
+        public void Setup(AppDbContext db, Controller _)
+        {
+            DbContext = db;
+        }
+
+        public void ValidateViewModel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
