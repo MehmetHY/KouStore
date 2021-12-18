c9a5a71a70338c49068ec94b206925dbf123e34f
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-18 22:36:30 +0300

    created product page view model

---
+++ KouStore/Areas/Customer/Models/ProductPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using KouStore.Models;
+
+namespace KouStore.Areas.Customer.Models
+{
+    public class ProductPageViewModel
+    {
+        public ProductModel Product { get; set; }
+        public CustomerModel? Customer { get; set; }
+        public bool IsInCard { get; set; } = false;
+        public ProductPageViewModel(ProductModel product, CustomerModel? customer = null, bool isInCard = false)
+        {
+            Product = product;
+            Customer = customer;
+            IsInCard = isInCard;
+        }
+    }
+}
