899ae6bb63b01016d627fa62a5efb99475a56d92
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-18 22:08:07 +0300

    created category page view model

--- KouStore/Areas/Customer/Models/CategoryPageViewModel.cs
+++ KouStore/Areas/Customer/Models/CategoryPageViewModel.cs
@@ -1,11 +1,11 @@
 ﻿using KouStore.Models.Absracts;
 using KouStore.Models;
-using KouStore.Config;
 
 namespace KouStore.Areas.Customer.Models
 {
     public class CategoryPageViewModel : QueryPageViewModel<ProductModel>
     {
         public override List<ProductModel> QueryModels { get; set; } = new();
+        public CategoryModel Category { get; set; } = new CategoryModel();
     }
 }
