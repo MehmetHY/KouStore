eb285dfd04fcfbd78bcf0d1afc3d210c0f93b1e6
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-19 11:54:49 +0300

    created search page view model

---
+++ KouStore/Areas/Customer/Models/SearchPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using KouStore.Models.Absracts;
+using KouStore.Models;
+
+namespace KouStore.Areas.Customer.Models
+{
+    public class SearchPageViewModel : QueryPageViewModel<ProductModel>
+    {
+        public override List<ProductModel> QueryModels { get; set; } = new();
+    }
+}
