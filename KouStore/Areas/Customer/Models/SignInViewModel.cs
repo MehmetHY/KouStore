9e5efdda148db743d16c3ae3cbf47efca9cbd2ad
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-19 14:28:19 +0300

    created sign in view model

---
+++ KouStore/Areas/Customer/Models/SignInViewModel.cs
@@ -0,0 +1,67 @@
+﻿using KouStore.Data;
+using KouStore.Models.Interfaces;
+using KouStore.Models;
+using KouStore.Managers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KouStore.Areas.Customer.Models
+{
+    public class SignInViewModel : IFormViewModel
+    {
+        public CustomerModel Customer { get; set; } = new();
+        public bool NameValid { get; set; } = true;
+        public bool PasswordValid { get; set; } = true;
+        public string NameError { get; set; } = string.Empty;
+        public string PasswordError { get; set; } = string.Empty;
+        public AppDbContext? DbContext { get; set; }
+        public bool Result => NameValid &&  PasswordValid;
+
+        public void Setup(AppDbContext db, Controller _) { DbContext = db; }
+
+        public void ValidateViewModel()
+        {
+            TrimWhiteSpaceFromFields();
+            if (IsThereAnyEmptyField()) return;
+            CheckNameAndPasswordCorrect();
+        }
+        private void TrimWhiteSpaceFromFields()
+        {
+            Customer.Name = Customer.Name.Trim();
+            Customer.Password = Customer.Password.Trim();
+        }
+        private bool IsThereAnyEmptyField()
+        {
+            if (Customer.Name == string.Empty)
+            {
+                NameValid = false;
+                NameError = "Name cannot be empty!";
+                return true;
+            }
+            if (Customer.Password == string.Empty)
+            {
+                PasswordValid = false;
+                PasswordError = "Password cannot be empty!";
+                return true;
+            }
+            return false;
+        }
+        private void CheckNameAndPasswordCorrect()
+        {
+            var queryCustomer = CustomerDbManager.GetCustomer(Customer.Name, DbContext);
+            if (queryCustomer == null)
+            {
+                NameValid = false;
+                NameError = $"Name \"{Customer.Name}\" doesn't exist!";
+            }
+            else if (Customer.Password != queryCustomer.Password)
+            {
+                PasswordValid = false;
+                PasswordError = "Password is incorrect!";
+            }
+            else
+            {
+                Customer = queryCustomer;
+            }
+        }
+    }
+}
