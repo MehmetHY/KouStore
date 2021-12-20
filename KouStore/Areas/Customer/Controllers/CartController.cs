73edb3b4f075123b2873c4c3b02b3b0b73f317f9
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-20 12:43:11 +0300

    created cart controller

---
+++ KouStore/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KouStore.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        [Route("[Controller]/{id}")]
+        [HttpGet]
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+    }
+}
