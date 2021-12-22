78a487156cfbb98e964f38c9aae03619c15f8487
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-12-22 09:13:18 +0300

    implemented customer nanagement

---
+++ KouStore/Areas/Admin/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using KouStore.Data;
+using KouStore.Managers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KouStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CustomersController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CustomersController(AppDbContext db) { _db = db; }
+
+        [Route("[Area]/[Controller]")]
+        [Route("[Area]/[Controller]/[Action]")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_db.Customers.ToList()).ToAdminAuthAction(this);
+        }
+
+        [Route("[Area]/[Controller]/[Action]/{id}")]
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (!this.IsAdminSignedIn())
+                RedirectToAction("Index", "SignIn");
+            var customer = CustomerDbManager.GetCustomer(id, _db);
+            if (customer != null)
+                customer.DeleteRecord(_db);
+            return RedirectToAction(nameof(Index)).ToAdminAuthAction(this);
+        }
+    }
+}
